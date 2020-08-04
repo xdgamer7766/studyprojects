158ba318e01e19954a2e173eaaddcd9f902974a6
xdgamer7766 <43033742+xdgamer7766@users.noreply.github.com>
2020-08-04 21:51:15 +0200

    welcome message

--- example_bot.py
+++ example_bot.py
@@ -14,4 +14,11 @@ async def on_message(message):
     if message.content.startswith('§hello'):
         await message.channel.send('Hello!')
 
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        'Szeva cigó, kössz hogy betoppantál a szerverre'
+    )
+
 client.run('NzQwMjQ0Mzc5MzkzOTgyNDk2.XymMTQ.Xn2mNyXox1i_Cmaer9fAwXawaGs')
