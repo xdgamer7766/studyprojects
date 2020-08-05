1528e2b951f8328f314c28d897bcc68fd65f6c57
xdgamer7766 <43033742+xdgamer7766@users.noreply.github.com>
2020-08-05 21:47:55 +0200

    bot prefix is under new management

--- example_bot.py
+++ example_bot.py
@@ -1,18 +1,15 @@
 import discord
+from discord.ext import commands
 
-client = discord.Client()
+client = commands.Bot(command_prefix='.')
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('§hello'):
-        await message.channel.send('Hello!')
+@client.command()
+async def hello(ctx):
+        await ctx.send('Hello!')
 
 @client.event
 async def on_member_join(member):
